e997b0358bd81278a11e6ddc568161f13f08a452
Dan Jones <dan@medology.com>
2017-10-19 16:07:06 -0500

    Pre-load source

--- watcher/js/app.js
+++ watcher/js/app.js
@@ -10,12 +10,19 @@
     },
     created: function() {
       axios.get('data/sources.json')
-        .then(response => this.sources = response.data);
+        .then(response => {
+          this.sources = response.data;
+          let hash = location.hash.split('#')[1];
+
+          let slugs = this.sources.map(source => source.slug);
+          if (hash && (slugs.indexOf(hash) > -1)) {
+            this.load(this.sources.filter(source => source.slug == hash)[0]);
+          }
+        });
     },
     methods: {
       load: function(source) {
         this.current = source;
-        console.log(source);
         axios.get('data/' + source.slug + '.json')
           .then(response => this.list = response.data);
       }
